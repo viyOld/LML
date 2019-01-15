459f89aa9ce1925d599ebcc9369fd3e43697cf9e
viy.2005@gmail.com <viy.2005@gmail.com>
2019-01-15 08:31:14 +0200

    00001

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func init() {
@@ -30,10 +32,56 @@ func readStartDb() {
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		if scanner.Text() == "" {
-			fmt.Println("----------------------------")
+		//читаємо строку з файла
+		s := strings.TrimSpace(scanner.Text())
+		if s == "" {
+			continue
 		}
-		fmt.Println(scanner.Text())
+		if strings.HasPrefix(s, "Number: ") {
+			//s = strings.TrimLeft(s, "Number:")
+			s = strings.TrimPrefix(s, "Number: ")
+			//i, err := strconv.Parse (s, 10, 64)
+			if n, err := strconv.Atoi(s); err == nil {
+				fmt.Printf("%T, %v\n", n, n)
+			} else {
+				fmt.Println(err)
+			}
+			continue
+		}
+		if strings.HasPrefix(s, "Name: ") {
+			s = strings.TrimPrefix(s, "Name: ")
+			fmt.Println(s)
+			continue
+		}
+		if strings.HasPrefix(s, "Homepage: ") {
+			s = strings.TrimPrefix(s, "Homepage: ")
+			fmt.Println(s)
+			continue
+		}
+		if strings.HasPrefix(s, "Download: ") {
+			s = strings.TrimPrefix(s, "Download: ")
+			fmt.Println(s)
+			continue
+		}
+		if strings.HasPrefix(s, "Wikipedia: ") {
+			s = strings.TrimPrefix(s, "Wikipedia: ")
+			fmt.Println(s)
+			continue
+		}
+		if strings.HasPrefix(s, "Distrowatch: ") {
+			s = strings.TrimPrefix(s, "Distrowatch: ")
+			fmt.Println(s)
+			continue
+		}
+		if strings.HasPrefix(s, "Size (mebibytes):") {
+			s = strings.TrimPrefix(s, "Size (mebibytes):")
+			s = strings.TrimSpace(s)
+			// s = split  strings.Split("a,b,c", ",")
+			mapStr := strings.Split(s, "-")
+			fmt.Println(mapStr)
+			continue
+		}
+		//fmt.Println("----------------------------")
 	}
 
 	if err := scanner.Err(); err != nil {
